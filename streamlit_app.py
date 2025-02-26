a760edbe2d730d7513460e1a0bb404ae7ebfe3c8
falcant <felix.alcantara@live.com>
2025-02-25 22:28:22 -0700

    step 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
-st.title("Hola que tal!")
+#st.title("Hola que tal!")
+
+# --- PAGE SETUP ---
+about_page = st.Page(
+    "views/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True,
+)
+project_1_page = st.Page(
+    "views/resources.py",
+    title="resources",
+    icon=":material/bar_chart:",
+)
+project_2_page = st.Page(
+    "views/events.py",
+    title="Events",
+    icon=":material/smart_toy:",
+)
+
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+pg.run()
