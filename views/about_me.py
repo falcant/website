8ba7ada29527089e9984d133da5048cc6b2605c7
falcant <felix.alcantara@live.com>
2025-02-27 21:44:15 -0700

    Adding more information in the social media and about_me files

--- views/about_me.py
+++ views/about_me.py
@@ -1,3 +1,31 @@
 import streamlit as st
 
-st.title("about me")
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    st.image("./assets/wtc_circle.png", width=300)
+
+with col2:
+    st.title("Wasatch Trails Collective", anchor=False)
+    st.write(
+        "A group for runners of color in the Salt Lake Area. ⛰️Eastern Shoshone, Goshute, and Núu-agha-tʉvʉ-pʉ̱ (Ute) Lands" )
+    st.write("Se habla Español.") 
+    #st.write(" ⛰️Eastern Shoshone, Goshute, and Núu-agha-tʉvʉ-pʉ̱ (Ute) Lands")
+
+# --- ABOUT US ---
+st.write("\n")
+st.subheader("About the WTC", anchor=False)
+st.write(
+    """
+    'About us' content
+    """
+)
+
+# --- Mission Statement ---
+st.write("\n")
+st.subheader("WTC Mission Statement", anchor=False)
+st.write(
+    """
+    'Mission Statement' Content
+    """
+)
--- views/socialmedia.py
+++ views/socialmedia.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title("Social Media")
+#st.title("Social Media")
+
+# --- ABOUT US ---
+st.write("\n")
+st.subheader("Follow us on Social Media", anchor=False)
+st.write(
+    """
+    - Instagram:
+    - Discord:
+    - Facebook:
+    - TikTok:
+    """
+)
